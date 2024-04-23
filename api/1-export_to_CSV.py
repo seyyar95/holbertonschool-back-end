8941a094407c608e2c6e1e35f78a0825b96d948b
seyyar95 <heydrovsyyar@gmail.com>
2024-04-23 09:55:31 +0400

    Function name modified and double quotes added

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -25,7 +25,7 @@ def export_to_csv():
     for task in tasks:
         title = task['title']
         status = task['completed']
-        string += f"{argv[1]},{employee_name},{status},{title}\n"
+        string += f'"{argv[1]}","{employee_name}","{status}","{title}"\n'
 
     filename = argv[1] + '.csv'
 
@@ -36,4 +36,4 @@ def export_to_csv():
 
 if __name__ == "__main__":
     if len(argv) == 2:
-        gather_user_information()
+        export_to_csv()
